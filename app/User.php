06a4c7534d50936b5fcd615896f31b6a702dcd05
George Jacob <kooldrjay060@gmail.com>
2019-06-24 02:59:56 +0100

    design for dashbaord

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function fullname(){
+        return $this->firstName." ". $this->lastName;
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="columns">
+        <div class="column">
+            <div class="card ">
+                <div class="card-image is-centered">
+                    <figure class="image profile-image force-center mt-4">
+                        <img class="is-rounded " src="/images/static/anon.jpg" alt="{{Auth::user()->fullname() }}">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media-content ">
+                        <h1 class="title centering-text">{{Auth::user()->fullname() }}</h1>
+                        <p class="subtile centering-text">{{ Auth::user()->email}}</p>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="column"></div>
     </div>
 </div>
 @endsection
